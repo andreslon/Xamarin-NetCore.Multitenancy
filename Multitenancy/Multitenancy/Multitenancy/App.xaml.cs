780cddd8e4715f2415de84d06d375171ac3a95df
Andreslon <andreslon@outlook.com>
2021-02-18 22:28:22 -0500

    Created base project

---
+++ Multitenancy/Multitenancy/Multitenancy/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Multitenancy.Services;
+using Multitenancy.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Multitenancy
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Multitenancy/Multitenancy/Multitenancy/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Multitenancy.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Multitenancy.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
