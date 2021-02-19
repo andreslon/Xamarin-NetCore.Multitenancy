efaeafd9acdc00590cffa68806155d77f499e9db
Andreslon <andreslon@outlook.com>
2021-02-18 23:21:44 -0500

    Added settingservice

---
+++ Multitenancy/Multitenancy/Multitenancy/Services/SettingService.cs
@@ -0,0 +1,16 @@
+﻿using Multitenancy.Core.Interfaces;
+using Multitenancy.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Multitenancy.Services
+{
+    public class SettingService : ISettingService
+    {
+        public string ProjectId()
+        {
+            return Secrets.ProjectId;
+        }
+    }
+}
